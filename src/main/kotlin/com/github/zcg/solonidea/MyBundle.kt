6e9a99f12763efec606869403d32b86a2d5d9db4
GitHub Action <action@github.com>
2023-02-13 14:41:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/zcg/solonidea/MyBundle.kt
+++ src/main/kotlin/com/github/zcg/solonidea/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.zcg.solonidea
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/zcg/solonidea/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zcg/solonidea/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zcg.solonidea.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zcg.solonidea.MyBundle
 
 class MyProjectService(project: Project) {
 
